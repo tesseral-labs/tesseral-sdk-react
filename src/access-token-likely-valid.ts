b52f1e88caa6839143489a4cbc147043d2a76f93
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2025-06-03 08:53:36 -0700

    Consolidate `parseAccessToken` usage (#12)
    Follow up to #11 to use the new `parseAccessToken` method everywhere.

--- src/access-token-likely-valid.ts
+++ src/access-token-likely-valid.ts
@@ -1,6 +1,6 @@
-import { AccessTokenClaims } from "@tesseral/tesseral-vanilla-clientside/api";
 import { useMemo } from "react";
 
+import { parseAccessToken } from "./parse-access-token";
 import { useDebouncedNow } from "./use-debounced-now";
 
 const ACCESS_TOKEN_EXPIRY_BUFFER_MILLIS = 10 * 1000;
@@ -14,10 +14,4 @@ export function useAccessTokenLikelyValid(accessToken: string): boolean {
     const parsedAccessToken = parseAccessToken(accessToken);
     return parsedAccessToken.exp! * 1000 > now + ACCESS_TOKEN_EXPIRY_BUFFER_MILLIS;
   }, [accessToken, now]);
-}
-
-function parseAccessToken(accessToken: string): AccessTokenClaims {
-  const claimsPart = accessToken.split(".")[1];
-  const decodedClaims = new TextDecoder().decode(Uint8Array.from(atob(claimsPart), (c) => c.charCodeAt(0)));
-  return JSON.parse(decodedClaims) as AccessTokenClaims;
 }
