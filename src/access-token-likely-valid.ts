61f853c3ff993c6e722b47bb1ccd64dfe8d022ce
Ulysse Carion <ulysse.carion@ssoready.com>
2025-06-19 09:28:15 -0700

    Suspend on expired access tokens

--- src/access-token-likely-valid.ts
+++ src/access-token-likely-valid.ts
@@ -5,13 +5,35 @@ import { useDebouncedNow } from "./use-debounced-now";
 
 const ACCESS_TOKEN_EXPIRY_BUFFER_MILLIS = 10 * 1000;
 
+/**
+ * Returns true if the access token is well-formed and expires far enough in
+ * the future.
+ *
+ * @param accessToken
+ */
 export function useAccessTokenLikelyValid(accessToken: string): boolean {
-  const now = useDebouncedNow(10 * 1000); // re-check expiration every 10 seconds
+  const now = useDebouncedNow(2 * 1000); // re-check expiration every 2 seconds
   return useMemo(() => {
     if (!accessToken) {
       return false;
     }
     const parsedAccessToken = parseAccessToken(accessToken);
     return parsedAccessToken.exp! * 1000 > now + ACCESS_TOKEN_EXPIRY_BUFFER_MILLIS;
+  }, [accessToken, now]);
+}
+
+/**
+ * Returns true if the access token is ill-formed or has expired.
+ *
+ * @param accessToken
+ */
+export function useAccessTokenLikelyExpired(accessToken: string): boolean {
+  const now = useDebouncedNow(2 * 1000); // re-check expiration every 2 seconds
+  return useMemo(() => {
+    if (!accessToken) {
+      return false;
+    }
+    const parsedAccessToken = parseAccessToken(accessToken);
+    return parsedAccessToken.exp! * 1000 < now;
   }, [accessToken, now]);
 }
